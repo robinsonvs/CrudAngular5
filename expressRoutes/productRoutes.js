045f388ec1a928480ed2f9495db1343f1830edf2
Robinson Severo <robinsonvs@gmail.com>
2018-04-30 12:21:21 -0300

    commit crud angular 5 30042018 1221

--- expressRoutes/productRoutes.js
+++ expressRoutes/productRoutes.js
@@ -2,5 +2,27 @@ const express = require('express')
 const app = express()
 const productRoutes = express.Router()
 
-const product = require('../models/Product')
+//require Item model in our routes module
+const Product = require('../models/Product')
+
+//define store routen
+productRoutes.route('/add').post(function(req, res){
+     const product = new Product(req.body)
+     product.save()
+     .then(item => {
+        res.status(200).json({'product': 'Product added sucessfully'})
+     })
+     .catch(err => {
+         res.status(400).send('Unable to save to database')
+     })
+})
+
+//define get data (index or listing) route
+productRoutes.route('/').get(function(req, res){
+    const id = req.params.id
+    Product.findById(id, function(err, product){
+        res.json(product)
+    })
+})
+
 
